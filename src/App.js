d7601b0489eb25d48d9cba5cec2b0931ec2c69c9
이기운 <slehdejfj@gmail.com>
2023-08-25 13:07:27 +0900

    변경2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         변경됨
+         변경됨2
         </p>
         <a
           className="App-link"
